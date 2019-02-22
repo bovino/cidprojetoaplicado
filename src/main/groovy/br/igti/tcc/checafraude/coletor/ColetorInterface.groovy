9e0e006cd1b282a6f158199e4eeb67cb5eb06b1c
bovino <bezerra.mj.listas@gmail.com>
2019-02-22 04:46:14 -0300

    evoluindo a interface base dos coletores

--- src/main/groovy/br/igti/tcc/checafraude/coletor/ColetorInterface.groovy
+++ src/main/groovy/br/igti/tcc/checafraude/coletor/ColetorInterface.groovy
@@ -1,5 +1,21 @@
 package br.igti.tcc.checafraude.coletor
 
+import com.fasterxml.jackson.databind.JsonNode
+
 interface ColetorInterface {
-  public void coletar()
+
+  /* Permite tomar ações ao final da coleta */
+  public void finalizarColeta()
+
+  /* Permite tomar ações ao final do processamento dos dados - ou seja ao final de tudo */
+  public void finalizarProcessamento()
+
+  /* Consulta e monta o objeto JsonNode root do ponto de percorrimento da árvore */
+  public void consultaJson()
+
+  // TODO evoluir para ter um impl com metodo default
+  public void registrarErrosColeta()
+
+  /* Recebe o JsonNode root e o percorre, executando o processamento, análise ou armazenamento dos dados para análises e processamentos posteriores */
+  public void processarDados(JsonNode meuNoAtual)
 }
